2967cfb887818afaf55ed019a274b01c3b6a664d
royalgambital951 <renukumawat.1228@gmail.com>
2026-02-28 16:35:21 +0530

    script.js
    Refactor splash screen and menu toggle functionality.

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword }
-from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 // Splash remove after 2 sec
-setTimeout(() => {
-    document.getElementById("splash").style.display = "none";
-}, 2000);
+window.addEventListener("load", function(){
+    setTimeout(() => {
+        document.getElementById("splash").style.display = "none";
+    }, 2000);
+});
 
-// Hamburger
+// Hamburger toggle
 const menuBtn = document.getElementById("menuBtn");
 const sideMenu = document.getElementById("sideMenu");
 
 menuBtn.addEventListener("click", () => {
-    sideMenu.style.left = sideMenu.style.left === "0px" ? "-250px" : "0px";
+    if(sideMenu.style.left === "0px"){
+        sideMenu.style.left = "-250px";
+    } else {
+        sideMenu.style.left = "0px";
+    }
 });
 
-// Logo zoom
+// Logo zoom modal
 const logoClick = document.getElementById("logoClick");
 const logoModal = document.getElementById("logoModal");
 
@@ -35,23 +27,4 @@ logoClick.addEventListener("click", () => {
 
 logoModal.addEventListener("click", () => {
     logoModal.style.display = "none";
-});
-
-// Auth
-document.getElementById("signup").addEventListener("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(() => alert("Signup Successful 👑"))
-    .catch(err => alert(err.message));
-});
-
-document.getElementById("login").addEventListener("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, password)
-    .then(() => alert("Login Successful 👑"))
-    .catch(err => alert(err.message));
 });
