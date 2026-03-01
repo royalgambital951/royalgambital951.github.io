a0786ce93b0dbc87771aafb9cef4501d9fd4aa79
royalgambital951 <renukumawat.1228@gmail.com>
2026-03-01 13:25:44 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-let logo = document.getElementById("logo");
-let popup = document.getElementById("popup");
-
-if(logo){
-logo.onclick = function(){
-popup.style.display="flex";
-}
-}
-
-if(popup){
-popup.onclick = function(){
-popup.style.display="none";
-}
-}
-
-});
-
 let music = new Audio("assets/bg-music.mp3");
 music.loop = true;
 
