6262dec60572fa10e7847772f05b3744d9b242ec
royalgambital951 <renukumawat.1228@gmail.com>
2026-03-01 13:28:06 +0530

    tic.js

--- js/tic.js
+++ js/tic.js
@@ -1,6 +1,5 @@
 let board = document.getElementById("board");
-let cells = [];
-let current="X";
+let cells=[];
 let steps=0;
 let time=0;
 
@@ -11,9 +10,7 @@ board.style.justifyContent="center";
 
 for(let i=0;i<9;i++){
 let cell=document.createElement("div");
-cell.style.width="100px";
 cell.style.height="100px";
-cell.style.background="white";
 cell.style.display="flex";
 cell.style.alignItems="center";
 cell.style.justifyContent="center";
@@ -29,22 +26,7 @@ if(cells[i].innerText==""){
 cells[i].innerText="X";
 steps++;
 document.getElementById("steps").innerText="Steps: "+steps;
-if(checkWin("X")){alert("You Win");confetti();return;}
-aiMove();
 }
-}
-
-function aiMove(){
-let empty=cells.map((c,i)=>c.innerText==""?i:null).filter(v=>v!=null);
-if(empty.length==0)return;
-let random=empty[Math.floor(Math.random()*empty.length)];
-cells[random].innerText="O";
-if(checkWin("O")){alert("AI Wins");}
-}
-
-function checkWin(p){
-let win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-return win.some(c=>c.every(i=>cells[i].innerText==p));
 }
 
 function restart(){
@@ -55,7 +37,3 @@ setInterval(()=>{
 time++;
 document.getElementById("timer").innerText="Time: "+time;
 },1000);
-
-function confetti(){
-alert("🎉 Congratulations!");
-}
